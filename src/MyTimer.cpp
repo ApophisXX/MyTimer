75c7cdd31831ad5ed051ba0fbc5bce37b46ffb9f
Robert <94802146+ApophisXX@users.noreply.github.com>
2023-10-11 23:36:06 +0200

    Add files via upload

--- src/MyTimer.cpp
+++ src/MyTimer.cpp
@@ -1,5 +1,5 @@
 /*
-  MyTimer.cpp - 
+  MyTimer.cpp -
 */
 
 #include "Arduino.h"
@@ -7,153 +7,112 @@
 
 MyTimer::MyTimer()
 {
-
 }
 
-bool MyTimer::POS(long Interval, unsigned long SystemMillis, bool Start){
+bool MyTimer::pos(long Interval, bool Start)
+{
   _Interval = Interval;
-  _currentMillis = SystemMillis;
+  _currentMillis = millis();
 
-if (Start == false) {
-  _previousMillis = _currentMillis;
+  if (Start == false)
+  {
+    _previousMillis = _currentMillis;
   }
 
-if ((_currentMillis - _previousMillis >= _Interval) && Start == true) {
+  if ((_currentMillis - _previousMillis >= _Interval) && Start == true)
+  {
     _previousMillis = _currentMillis;
     return true;
-  }else return false;
+  }
+  else
+    return false;
 }
 
-bool MyTimer::NEG(long Interval, unsigned long SystemMillis, bool Start){
-  _Interval = Interval;
-  _currentMillis = SystemMillis;
+bool MyTimer::setDelayedOn(long Delay, bool Start)
+{
+  _DelayMillis = Delay;
+  _currentMillis = millis();
+  _merker = Start;
 
-if (Start == false) {
-  _previousMillis = _currentMillis;
-  }
-
-if ((_currentMillis - _previousMillis >= _Interval) && Start == true) {
+  if (_merker == false)
+  {
     _previousMillis = _currentMillis;
     return false;
   }
-return true;
-}
 
-bool MyTimer::DELAY_ON(long DelayMillis, unsigned long SystemMillis, bool StartMerker){
-  _DelayMillis = DelayMillis;
-  _currentMillis = SystemMillis;
-  _merker = StartMerker;
-
-if(_merker == false){
-  _previousMillis = _currentMillis;
+  if (_currentMillis - _previousMillis >= _DelayMillis)
+  {
+    return true;
+  }
   return false;
 }
 
-if (_currentMillis - _previousMillis >= _DelayMillis) {
-    return true;
-  }
-return false;
-}
+bool MyTimer::blinkOnOff(long TimeOn, long TimeOff, bool Start)
+{
+  _IntervalON = TimeOn;
+  _IntervalOFF = TimeOff;
+  _currentMillis = millis();
 
-bool MyTimer::BLINK_BOOL(long Interval, unsigned long SystemMillis, bool Start){
-  _Interval = Interval;
-  _currentMillis = SystemMillis;
-
-if(Start == false){
-  _previousMillis = _currentMillis;
-  _merker = false;
-}
-
-if(Start == true){
-  if ((_currentMillis - _previousMillis >= _Interval) && _merker == false) {
-    _previousMillis = _currentMillis;
-    _merker = true;
-    return _merker;
-    }
-
-  if ((_currentMillis - _previousMillis >= _Interval) && _merker == true) {
+  if (Start == false)
+  {
     _previousMillis = _currentMillis;
     _merker = false;
-    return _merker;
-    }
-
   }
-return _merker;
-}
 
-bool MyTimer::BLINK_ON_OFF(long IntervalON, long IntervalOFF, unsigned long SystemMillis, bool Start){
-  _IntervalON = IntervalON;
-  _IntervalOFF = IntervalOFF;
-  _currentMillis = SystemMillis;
-
-if(Start == false){
-  _previousMillis = _currentMillis;
-  _merker = false;
-}
-
-if(Start == true){
-  if ((_currentMillis - _previousMillis >= _IntervalON) && _merker == false) {
-    _previousMillis = _currentMillis;
-    _merker = true;
-    return _merker;
+  if (Start == true)
+  {
+    if ((_currentMillis - _previousMillis >= _IntervalOFF) && _merker == false)
+    {
+      _previousMillis = _currentMillis;
+      _merker = true;
+      return _merker;
     }
 
-  if ((_currentMillis - _previousMillis >= _IntervalOFF) && _merker == true) {
-    _previousMillis = _currentMillis;
-    _merker = false;
-    return _merker;
+    if ((_currentMillis - _previousMillis >= _IntervalON) && _merker == true)
+    {
+      _previousMillis = _currentMillis;
+      _merker = false;
+      return _merker;
     }
-
   }
-return _merker;
+  return _merker;
 }
 
-int MyTimer::ADD(int LowerEnd, int UpperEnd, int StartValue, int IncrBy, long Interval, unsigned long SystemMillis, bool Start){
+int MyTimer::intervalAdd(int LowerEnd, int UpperEnd, int StartValue, int IncrBy, long Interval, bool Start)
+{
   _Interval = Interval;
-  _currentMillis = SystemMillis;
+  _currentMillis = millis();
 
-if (Start == false) {
-  _previousMillis = 0;
-  _value = StartValue;
-  _config = false;
-  return StartValue;
+  if (Start == false)
+  {
+    _previousMillis = 0;
+    _value = StartValue;
+    _config = false;
+    return StartValue;
   }
 
-if (Start == true && _config == false) {
-  _previousMillis = _currentMillis;
-  _value = StartValue +IncrBy;
-  _config = true;
+  if (Start == true && _config == false)
+  {
+    _previousMillis = _currentMillis;
+    _value = StartValue + IncrBy;
+    _config = true;
   }
 
-if ((_currentMillis - _previousMillis >= _Interval) && Start == true) {
+  if ((_currentMillis - _previousMillis >= _Interval) && Start == true)
+  {
     _previousMillis = _currentMillis;
     _value = _value + IncrBy;
   }
 
-if(_value > UpperEnd){
-_value = LowerEnd;
-}
-if(_value < LowerEnd){
-_value = UpperEnd;
-}
-return _value;
-}
-
-bool MyTimer::RANDOM_POS(unsigned long min, unsigned long max, unsigned long SystemMillis, bool Start){
- _currentMillis = SystemMillis;
-
-if (Start == false) {
-  _previousMillis = _currentMillis;
-  }  
-if (Start == true && _merker == false) {
-  _Interval = random(min, max);
-  _merker = true;
+  if (_value > UpperEnd)
+  {
+    _value = LowerEnd;
   }
-if ((_currentMillis - _previousMillis >= _Interval) && Start == true) {
-    _previousMillis = _currentMillis;
-    _merker = false;
-    return true;
-  }else return false;
+  if (_value < LowerEnd)
+  {
+    _value = UpperEnd;
+  }
+  return _value;
 }
 
 MyTimerPin::MyTimerPin(int pin)
@@ -162,29 +121,32 @@ MyTimerPin::MyTimerPin(int pin)
   _pin = pin;
 }
 
-void MyTimerPin::BLINK_PIN(long Interval, unsigned long SystemMillis, bool Start)
+void MyTimerPin::blinkPin(long Interval, bool Start)
 {
-   _Interval = Interval;
-   _currentMillis = SystemMillis;
+  _Interval = Interval;
+  _currentMillis = millis();
 
-   if(Start == false){
-  _previousMillis = _currentMillis;
-  _merker = false;
-}
-
-if(Start == true){
-  if ((_currentMillis - _previousMillis >= _Interval) && _merker == false) {
+  if (Start == false)
+  {
     _previousMillis = _currentMillis;
-    digitalWrite(_pin, HIGH);
-    _merker = true;
-    }
-
-  if ((_currentMillis - _previousMillis >= _Interval) && _merker == true) {
-    _previousMillis = _currentMillis;
-    digitalWrite(_pin, LOW);
     _merker = false;
+  }
+
+  if (Start == true)
+  {
+    if ((_currentMillis - _previousMillis >= _Interval) && _merker == false)
+    {
+      _previousMillis = _currentMillis;
+      digitalWrite(_pin, HIGH);
+      _merker = true;
     }
 
+    if ((_currentMillis - _previousMillis >= _Interval) && _merker == true)
+    {
+      _previousMillis = _currentMillis;
+      digitalWrite(_pin, LOW);
+      _merker = false;
+    }
   }
 }
 
@@ -194,23 +156,24 @@ MyTimerSerial::MyTimerSerial(HardwareSerial *serIn)
   _ser = serIn;
 }
 
-void MyTimerSerial::begin( long baud)
+void MyTimerSerial::begin(long baud)
 {
   _baud = baud;
-   _Serial = _ser;
+  _Serial = _ser;
   _Serial->begin(_baud);
   _Serial->println("Ready to Rip!");
 }
 
-void MyTimerSerial::IntervalSend(long Interval, unsigned long SystemMillis, String str)
+void MyTimerSerial::IntervalSend(long Interval, String str)
 {
-   _Interval = Interval;
-   _currentMillis = SystemMillis;
+  _Interval = Interval;
+  _currentMillis = millis();
 
-   if (_currentMillis - _previousMillis >= _Interval) {
+  if (_currentMillis - _previousMillis >= _Interval)
+  {
     _previousMillis = _currentMillis;
     _Serial->println(str);
-    } 
+  }
 }
 
 MyTimerButton::MyTimerButton(int pin, long CoolDown)
@@ -221,139 +184,235 @@ MyTimerButton::MyTimerButton(int pin, long CoolDown)
 
 void MyTimerButton::begin()
 {
-  if(_pin >= 0 && !_Begun)
-  pinMode(_pin, INPUT);
+  if (_pin >= 0 && !_Begun)
+    pinMode(_pin, INPUT);
   _Begun = true;
 }
 
 void MyTimerButton::beginPullUp()
 {
-  if(_pin > 0 && !_PullUpBegun)
-  pinMode(_pin, INPUT_PULLUP);
+  if (_pin > 0 && !_PullUpBegun)
+    pinMode(_pin, INPUT_PULLUP);
   _PullUpBegun = true;
 }
 
-void MyTimerButton::setSystemMillis(unsigned long SystemMillis){
-  _currentMillis = SystemMillis;
-}
-
 bool MyTimerButton::rising()
-{    
+{
   ri_pressed = false;
-  if(_PullUpBegun){ if(digitalRead(_pin) == LOW){ri_pressed = true;} }
-  if(_Begun){ if(digitalRead(_pin) == HIGH){ri_pressed = true;} }
+  _currentMillis = millis();
+  if (_PullUpBegun)
+  {
+    if (digitalRead(_pin) == LOW)
+    {
+      ri_pressed = true;
+    }
+  }
+  if (_Begun)
+  {
+    if (digitalRead(_pin) == HIGH)
+    {
+      ri_pressed = true;
+    }
+  }
 
-    if(ri_pressed && !ri_blocked){
-      ri_previousMillis = _currentMillis;
-      ri_blocked = true;
-      return true;
-      }
-    if ((_currentMillis - ri_previousMillis >= _CoolDown) && ri_blocked && !ri_pressed){
-      ri_blocked = false;
-      return false;
-      }
-    return false;   
+  if (ri_pressed && !ri_blocked)
+  {
+    ri_previousMillis = _currentMillis;
+    ri_blocked = true;
+    return true;
+  }
+  if ((_currentMillis - ri_previousMillis >= _CoolDown) && ri_blocked && !ri_pressed)
+  {
+    ri_blocked = false;
+    return false;
+  }
+  return false;
 }
 
 bool MyTimerButton::falling()
-{  
+{
   fa_pressed = false;
-  if(_PullUpBegun){ if(digitalRead(_pin) == LOW){fa_pressed = true;} }
-  if(_Begun){ if(digitalRead(_pin) == HIGH){fa_blocked = true;} }
-
-    if(fa_pressed && !fa_blocked){
-      fa_previousMillis = _currentMillis;
+  _currentMillis = millis();
+  if (_PullUpBegun)
+  {
+    if (digitalRead(_pin) == LOW)
+    {
+      fa_pressed = true;
+    }
+  }
+  if (_Begun)
+  {
+    if (digitalRead(_pin) == HIGH)
+    {
       fa_blocked = true;
-      return false;
-      }
-    if ((_currentMillis - fa_previousMillis >= _CoolDown) && fa_blocked && !fa_pressed){
-      fa_blocked = false;
-      return true;
-      }
-    return false;  
+    }
+  }
+
+  if (fa_pressed && !fa_blocked)
+  {
+    fa_previousMillis = _currentMillis;
+    fa_blocked = true;
+    return false;
+  }
+  if ((_currentMillis - fa_previousMillis >= _CoolDown) && fa_blocked && !fa_pressed)
+  {
+    fa_blocked = false;
+    return true;
+  }
+  return false;
 }
 
 bool MyTimerButton::pressed()
-{   
+{
   pr_pressed = false;
-  if(_PullUpBegun){ if(digitalRead(_pin) == LOW){pr_pressed = true;} }
-  if(_Begun){ if(digitalRead(_pin) == HIGH){pr_pressed = true;} }
+  _currentMillis = millis();
+  if (_PullUpBegun)
+  {
+    if (digitalRead(_pin) == LOW)
+    {
+      pr_pressed = true;
+    }
+  }
+  if (_Begun)
+  {
+    if (digitalRead(_pin) == HIGH)
+    {
+      pr_pressed = true;
+    }
+  }
 
-    if(pr_pressed){
-      pr_previousMillis = _currentMillis;
-      pr_blocked = true;
-      }
-    if ((_currentMillis - pr_previousMillis >= _CoolDown) && !pr_pressed){
-      pr_blocked = false;
-      }
-    if(pr_blocked) return true;
-    return false; 
+  if (pr_pressed)
+  {
+    pr_previousMillis = _currentMillis;
+    pr_blocked = true;
+  }
+  if ((_currentMillis - pr_previousMillis >= _CoolDown) && !pr_pressed)
+  {
+    pr_blocked = false;
+  }
+  if (pr_blocked)
+    return true;
+  return false;
 }
 
 bool MyTimerButton::released()
-{  
+{
   re_pressed = false;
-  if(_PullUpBegun){ if(digitalRead(_pin) == LOW){re_pressed = true;} }
-  if(_Begun){ if(digitalRead(_pin) == HIGH){re_pressed = true;} }
+  _currentMillis = millis();
+  if (_PullUpBegun)
+  {
+    if (digitalRead(_pin) == LOW)
+    {
+      re_pressed = true;
+    }
+  }
+  if (_Begun)
+  {
+    if (digitalRead(_pin) == HIGH)
+    {
+      re_pressed = true;
+    }
+  }
 
-    if(re_pressed){
-      re_previousMillis = _currentMillis;
-      re_blocked = true;
-      }
-    if ((_currentMillis - re_previousMillis >= _CoolDown) && !re_pressed){
-      re_blocked = false;;
-      }
-    if(pr_blocked) return false;
-    return true;  
+  if (re_pressed)
+  {
+    re_previousMillis = _currentMillis;
+    re_blocked = true;
+  }
+  if ((_currentMillis - re_previousMillis >= _CoolDown) && !re_pressed)
+  {
+    re_blocked = false;
+    ;
+  }
+  if (pr_blocked)
+    return false;
+  return true;
 }
 
 bool MyTimerButton::toggled()
-{  
+{
   to_pressed = false;
-  if(_PullUpBegun){ if(digitalRead(_pin) == LOW){to_pressed = true;} }
-  if(_Begun){ if(digitalRead(_pin) == HIGH){to_pressed = true;} }
+  _currentMillis = millis();
+  if (_PullUpBegun)
+  {
+    if (digitalRead(_pin) == LOW)
+    {
+      to_pressed = true;
+    }
+  }
+  if (_Begun)
+  {
+    if (digitalRead(_pin) == HIGH)
+    {
+      to_pressed = true;
+    }
+  }
 
-    if(to_pressed && !to_blocked){
-      to_previousMillis = _currentMillis;
-      to_blocked = true;
-      toggle = !toggle;
-      }
+  if (to_pressed && !to_blocked)
+  {
+    to_previousMillis = _currentMillis;
+    to_blocked = true;
+    toggle = !toggle;
+  }
 
+  if ((_currentMillis - to_previousMillis >= _CoolDown) && to_blocked && !to_pressed)
+  {
+    to_blocked = false;
+  }
 
-    if ((_currentMillis - to_previousMillis  >= _CoolDown) && to_blocked && !to_pressed){
-      to_blocked = false;
-      }
-
-    if(toggle == true){
-      return true;
-    }return false; 
+  if (toggle == true)
+  {
+    return true;
+  }
+  return false;
 }
 
 uint16_t MyTimerButton::modeSwitch(uint16_t LowerEnd, uint16_t StartFrom, uint16_t UpperEnd)
-{  
+{
   _LowerEnd = LowerEnd;
   _UpperEnd = UpperEnd;
   _StartFrom = StartFrom;
-  if(initalised == false)
+  _currentMillis = millis();
+  if (initalised == false)
   {
     initalised = true;
     _mode = _StartFrom;
   }
   mo_pressed = false;
-  if(_PullUpBegun){ if(digitalRead(_pin) == LOW){mo_pressed = true;} }
-  if(_Begun){ if(digitalRead(_pin) == HIGH){mo_pressed = true;} }
+  if (_PullUpBegun)
+  {
+    if (digitalRead(_pin) == LOW)
+    {
+      mo_pressed = true;
+    }
+  }
+  if (_Begun)
+  {
+    if (digitalRead(_pin) == HIGH)
+    {
+      mo_pressed = true;
+    }
+  }
 
-    if(mo_pressed && !mo_blocked){
-      mo_previousMillis = _currentMillis;
-      mo_blocked = true;
-      _mode = _mode + 1;
-      }
-    if(_mode > _UpperEnd){_mode = _LowerEnd;}
-    if(_mode < _LowerEnd){_mode = _UpperEnd;}
+  if (mo_pressed && !mo_blocked)
+  {
+    mo_previousMillis = _currentMillis;
+    mo_blocked = true;
+    _mode = _mode + 1;
+  }
+  if (_mode > _UpperEnd)
+  {
+    _mode = _LowerEnd;
+  }
+  if (_mode < _LowerEnd)
+  {
+    _mode = _UpperEnd;
+  }
 
-    if ((_currentMillis - mo_previousMillis  >= _CoolDown) && mo_blocked && !mo_pressed){
-      mo_blocked = false;
-      }
+  if ((_currentMillis - mo_previousMillis >= _CoolDown) && mo_blocked && !mo_pressed)
+  {
+    mo_blocked = false;
+  }
 
-    return _mode; 
+  return _mode;
 }
--- src/MyTimer.h
+++ src/MyTimer.h
@@ -8,91 +8,88 @@
 
 class MyTimer
 {
-  public:
-    MyTimer();
-    bool POS(long Interval, unsigned long SystemMillis, bool Start);
-    bool NEG(long Interval, unsigned long SystemMillis, bool Start);
-    bool DELAY_ON(long DelayMillis, unsigned long SystemMillis, bool StartMerker);
-    bool BLINK_BOOL(long Interval, unsigned long SystemMillis, bool Start);
-    bool BLINK_ON_OFF(long IntervalON, long IntervalOFF, unsigned long SystemMillis, bool Merker);
-    int ADD(int LowerEnd, int UpperEnd, int StartValue, int IncrBy, long Interval, unsigned long SystemMillis, bool Start);
-    bool RANDOM_POS(unsigned long min, unsigned long max, unsigned long SystemMillis, bool Start);
+public:
+  MyTimer();
+  bool pos(long Interval, bool Start);
+  bool setDelayedOn(long Delay, bool Start);
+  bool blinkOnOff(long TimeOn, long TimeOff, bool Start);
+  int intervalAdd(int LowerEnd, int UpperEnd, int StartValue, int IncrBy, long Interval, bool Start);
 
-  private:
-    uint16_t _value;
-    unsigned long _Interval, _IntervalON, _IntervalOFF, _DelayMillis;
-    unsigned long _currentMillis;
-    unsigned long _previousMillis = 0;
-    bool _merker, _config;
-    int _pin;
+private:
+  uint16_t _value;
+  unsigned long _Interval, _IntervalON, _IntervalOFF, _DelayMillis;
+  unsigned long _currentMillis;
+  unsigned long _previousMillis = 0;
+  bool _merker, _config;
+  int _pin;
 };
 
 class MyTimerPin
 {
-  public:
-    MyTimerPin(int pin);
-    void BLINK_PIN(long Interval, unsigned long SystemMillis, bool Start);
+public:
+  MyTimerPin(int pin);
+  void blinkPin(long Interval, bool Start);
 
-  private:
-    unsigned long _Interval;
-    unsigned long _currentMillis;
-    unsigned long _previousMillis = 0;
-    bool _merker;
-    int _pin;
+private:
+  unsigned long _Interval;
+  unsigned long _currentMillis;
+  unsigned long _previousMillis = 0;
+  bool _merker;
+  int _pin;
 };
 
 class MyTimerSerial
 {
-  public:
-    MyTimerSerial(HardwareSerial *serIn);
-    void begin( long baud);
-    void IntervalSend(long Interval, unsigned long SystemMillis, String str);    
-  private:
-    HardwareSerial *_Serial, *_ser;
-    long _baud;
-    unsigned long _Interval;
-    unsigned long _currentMillis;
-    unsigned long _previousMillis = 0;
+public:
+  MyTimerSerial(HardwareSerial *serIn);
+  void begin(long baud);
+  void IntervalSend(long Interval, String str);
+
+private:
+  HardwareSerial *_Serial, *_ser;
+  long _baud;
+  unsigned long _Interval;
+  unsigned long _currentMillis;
+  unsigned long _previousMillis = 0;
 };
 
 class MyTimerButton
 {
-  
-  public:
-    MyTimerButton(int pin, long CoolDown);
-    void begin();
-    void beginPullUp();
-    void setSystemMillis(unsigned long SystemMillis);
-    bool rising();
-    bool falling();
-    bool pressed();
-    bool released();
-    bool toggled();
-    uint16_t modeSwitch(uint16_t LowerEnd, uint16_t StartFrom, uint16_t UpperEnd);
 
-  private:
-    int _pin = 0;
-    unsigned long _CoolDown, _currentMillis;
-    bool _PullUpBegun, _Begun = false;
-    
-    bool pr_blocked, pr_pressed = false; //void pressed
-    unsigned long pr_previousMillis = 0;
+public:
+  MyTimerButton(int pin, long CoolDown);
+  void begin();
+  void beginPullUp();
+  bool rising();
+  bool falling();
+  bool pressed();
+  bool released();
+  bool toggled();
+  uint16_t modeSwitch(uint16_t LowerEnd, uint16_t StartFrom, uint16_t UpperEnd);
 
-    bool re_blocked, re_pressed = false; //void release
-    unsigned long re_previousMillis = 0;
+private:
+  int _pin = 0;
+  unsigned long _CoolDown, _currentMillis;
+  bool _PullUpBegun, _Begun = false;
 
-    bool ri_blocked, ri_pressed = false; //void isPressed
-    unsigned long ri_previousMillis = 0;
+  bool pr_blocked, pr_pressed = false; // void pressed
+  unsigned long pr_previousMillis = 0;
 
-    bool fa_blocked, fa_pressed = false; //void isRelease
-    unsigned long fa_previousMillis = 0;
+  bool re_blocked, re_pressed = false; // void release
+  unsigned long re_previousMillis = 0;
 
-    bool to_blocked, to_pressed, toggle = false;  //void toggled
-    unsigned long to_previousMillis = 0;
+  bool ri_blocked, ri_pressed = false; // void isPressed
+  unsigned long ri_previousMillis = 0;
 
-    bool mo_blocked, mo_pressed, initalised = false;  //void modeSwitch
-    uint16_t _mode, _LowerEnd, _StartFrom, _UpperEnd = 0;
-    unsigned long mo_previousMillis = 0;
+  bool fa_blocked, fa_pressed = false; // void isRelease
+  unsigned long fa_previousMillis = 0;
+
+  bool to_blocked, to_pressed, toggle = false; // void toggled
+  unsigned long to_previousMillis = 0;
+
+  bool mo_blocked, mo_pressed, initalised = false; // void modeSwitch
+  uint16_t _mode, _LowerEnd, _StartFrom, _UpperEnd = 0;
+  unsigned long mo_previousMillis = 0;
 };
 
 #endif
